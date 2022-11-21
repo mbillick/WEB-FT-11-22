ffe654ca18723215e0f2f6fb2c21efdb3006e16c
mbillick <118167007+mbillick@users.noreply.github.com>
2022-11-21 10:48:08 -0600

    practice

--- Week2/Day1/Classes.py
+++ Week2/Day1/Classes.py
@@ -1,1 +1,30 @@
 print("hello world classes")
+# funtion
+# boolean
+# floats
+# interger
+# list/arrays
+# dictionary
+# tuples (12323, 123123123, 213123)
+
+# classes
+
+#  how to define a class in Python
+# def printme():
+
+class Student:
+     def __init__(self,name,age,ghusername):
+      self.name = name
+      self.age = name
+      self.ghusername = name
+    def greeting(self):
+        print("Hello my name is")
+        #invoking a class
+        #instantiating
+        
+joe = Student("joe",12,"jwfrasier")
+Vinny = Studen("vinny","20,"vinny@gh")
+#dot notation
+   
+   print(joe.age)
+   vinny,greeting("vinny")
---
+++ Week2/Day1/classes2.py
@@ -0,0 +1,26 @@
+#Pets
+
+# make a noise
+# def animalSound()
+# have an owner
+#def whoIsMyOwner()
+# have a name
+
+# whatsMyName
+#def whatsMy Name
+# let us know what kind of pet they are
+
+#let us 
+
+class Pet:
+     def __init__(self,noise,owner,name):
+      self.noise = noise
+      self.owner = owner
+      self.name = name
+     
+     def greeting(self):
+        print("the noise i make is") 
+
+ Dog = Pet("Bark","Michael","Pupp")
+
+print(self.noise):
